92bbd017b7e3e39315b695520824aba6ae944cc4
law23sum <94133747+law23sum@users.noreply.github.com>
2023-04-20 08:40:08 -0400

    files working full test and reports

---
+++ src/test/java/com/company/steps/WebAppActions.java
@@ -0,0 +1,21 @@
+package com.company.steps;
+
+import net.thucydides.core.annotations.Step;
+
+public class WebAppActions {
+    
+    @Step("Open the application")
+    public void openApplication() {
+        // Implement the code to open the application
+    }
+    
+    @Step("Perform some action")
+    public void performAction() {
+        // Implement the code to perform the action
+    }
+    
+    @Step("Verify the result")
+    public void verifyResult() {
+        // Implement the code to verify the expected result
+    }
+}
